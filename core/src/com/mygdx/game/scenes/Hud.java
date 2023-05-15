8ff3748515c92bea5d5cb5ae1dde0aaf624afec3
KasimovDanil637 <113104558+KasimovDanil637@users.noreply.github.com>
2023-05-15 19:49:14 +0300

    Bullet create changes WorldContactListener

--- core/src/com/mygdx/game/scenes/Hud.java
+++ core/src/com/mygdx/game/scenes/Hud.java
@@ -4,23 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.DistanceFieldFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Bits;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.constants.Constants;
+import com.mygdx.game.entity.player.PlayerData;
+import com.mygdx.game.entity.bullet.BulletData;
+import com.mygdx.game.entity.player.PlayerProvider;
+
+import java.awt.*;
 
 public class Hud implements Disposable{
 
     //Scene2D.ui Stage and its own Viewport for HUD
     public Stage stage;
     private Viewport viewport;
+    private PlayerData playerData;
 
     //Mario score/time Tracking Variables
     private Integer worldTimer;
+    private static Integer health;
     private boolean timeUp; // true when the world timer reaches 0
     private float timeCount;
     private static Integer score;
@@ -32,11 +43,13 @@ public class Hud implements Disposable{
     private Label levelLabel;
     private Label worldLabel;
     private Label marioLabel;
+    private static Label healthLabel;
 
-    public Hud(SpriteBatch sb){
-        //define our tracking variables
+    public Hud(SpriteBatch sb, PlayerData playerData){
+        //define our tracking variables;
         worldTimer = 300;
         timeCount = 0;
+        this.playerData = playerData;
         score = 0;
 
 
@@ -47,46 +60,36 @@ public class Hud implements Disposable{
 
         //define a table used to organize our hud's labels
         Table table = new Table();
+        Table table1 = new Table();
         //Top-Align table
         table.top();
         //make the table fill the entire stage
         table.setFillParent(true);
 
+
         //define our labels using the String, and a Label style consisting of a font and color
-        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        worldLabel = new Label("Test world", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-
+        healthLabel = new Label(String.format("%02d", (int) playerData.getHp()), new Label.LabelStyle(new BitmapFont(), Color.RED));
+        scoreLabel = new Label(String.format("%01d", score), new Label.LabelStyle(new BitmapFont(), Color.YELLOW));
         //add our labels to our table, padding the top, and giving them all equal width with expandX
-        table.add(worldLabel).expandX().padTop(10);
-        table.add(timeLabel).expandX().padTop(10);
-        //add a second row to our table
-        table.row();
-        table.add(scoreLabel).expandX();
-        table.add(levelLabel).expandX();
-        table.add(countdownLabel).expandX();
-
+        table.add(healthLabel).expandX().pad(170,320,0,0);
+        table1.add(scoreLabel).expandX().pad(100,320,0,0);
         //add our table to the stage
         stage.addActor(table);
+        stage.addActor(table1);
 
     }
 
-    public void update(float dt){
-        timeCount += dt;
-        if(timeCount >= 1){
-            if (worldTimer > 0) {
-                worldTimer--;
-            } else {
-                timeUp = true;
-            }
-            countdownLabel.setText(String.format("%03d", worldTimer));
-            timeCount = 0;
-        }
+
+
+    public void update(){
+        Integer hp = (int) playerData.getHp();
+        healthLabel.setText(String.format("%01d", hp));
     }
+
 
     @Override
     public void dispose() { stage.dispose(); }
 
     public boolean isTimeUp() { return timeUp; }
+
 }
